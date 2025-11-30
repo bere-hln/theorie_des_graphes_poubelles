cc8149e88ca1e605660cd1b6dccb19967ea195ef
bere <berenice.helion@edu.ece.fr>
2025-11-30 08:40:14 +0100

    maj sommet

--- src/Sommet.java
+++ src/Sommet.java
@@ -1,2 +1,17 @@
 public class Sommet {
+    private int  id;
+    private String nom;
+
+    public Sommet(int id, String nom) {
+        this.id = id;
+        this.nom = nom;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
 }
