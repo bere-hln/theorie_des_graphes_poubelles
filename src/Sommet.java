be93539439401383d3c15597553901ff884a80f5
gouia <gouiaanour45@gmail.com>
2025-12-06 23:22:44 +0100

    sommet modif

--- src/Sommet.java
+++ src/Sommet.java
@@ -1,10 +1,14 @@
 public class Sommet {
     private int  id;
     private String nom;
+    //thème 3
+    private int quantite = 0;
+    private int couleur = -1;
 
     public Sommet(int id, String nom) {
         this.id = id;
         this.nom = nom;
+
     }
 
     public int getId() {
@@ -14,6 +18,15 @@ public class Sommet {
     public String getNom() {
         return nom;
     }
+
+    //thème 3
+    public int getQuantite() {return quantite;}
+
+    public void setQuantite(int q) {this.quantite = q;}
+
+    public int getCouleur() {return couleur;}
+
+    public void setCouleur(int couleur) {this.couleur = couleur;}
 
     @Override
     public String toString() {
